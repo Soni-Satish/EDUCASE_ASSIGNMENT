f7dcce8cc5810a16dc8cf55e3eea97307b408df8
santosh kumar soni <santosh.soni@legalpay.in>
2024-04-09 21:05:49 +0530

    Updates

---
+++ precache-manifest.54da7dec153cf3b20a6bea77d441d68a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6873a881b6c8ddbf6bf0904f56137ee9",
+    "url": "/EDUCASE_ASSIGNMENT/index.html"
+  },
+  {
+    "revision": "2f14c2abd07d174f41d7",
+    "url": "/EDUCASE_ASSIGNMENT/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "8ab93390cf5e90465325",
+    "url": "/EDUCASE_ASSIGNMENT/static/css/main.c3672a5e.chunk.css"
+  },
+  {
+    "revision": "2f14c2abd07d174f41d7",
+    "url": "/EDUCASE_ASSIGNMENT/static/js/2.3320711a.chunk.js"
+  },
+  {
+    "revision": "6d0282405e960cb9fa3d738e1656b9ce",
+    "url": "/EDUCASE_ASSIGNMENT/static/js/2.3320711a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ab93390cf5e90465325",
+    "url": "/EDUCASE_ASSIGNMENT/static/js/main.aa127fab.chunk.js"
+  },
+  {
+    "revision": "6adda09ddcbfe0f1c95f",
+    "url": "/EDUCASE_ASSIGNMENT/static/js/runtime-main.50662577.js"
+  },
+  {
+    "revision": "c9255dd156855734f7d5954b6c63c7b7",
+    "url": "/EDUCASE_ASSIGNMENT/static/media/Ellipse 114@2x.c9255dd1.png"
+  },
+  {
+    "revision": "70fe85d07b79f0d030e5f5d791ba3b5c",
+    "url": "/EDUCASE_ASSIGNMENT/static/media/camera.70fe85d0.svg"
+  }
+]);
